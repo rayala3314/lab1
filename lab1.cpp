d2abcc6ce75fe0a86bbd89d7948c5850589419d9
rayala3314 <rayala19@csub.edu>
2020-08-28 11:35:03 -0700

    my first commit for lab-1

--- lab1.cpp
+++ lab1.cpp
@@ -9,14 +9,31 @@ bool checkWord(string word);
 int main()
 {
 	string words[6] = {"a", "aardvark", "book", "fall", "abcdddddd", "test"};
+	string word;
+	bool check = true;
+	
+    do {
+    	cout << "Enter 'test' to test the program";
+		cout << "\nPlease enter a word: ";
+		cin >> word;
+		check = checkWord(word);
+	
+        if (check)
+			cout << "Word has two consecutive letters that are the same\n";
+		if (word == "test") {
 
-	for (int i = 0; i < 6; i++) {
-		bool check = checkWord(words[i]);
-		if (check)
-			cout << setw(10) << words[i] << setw(10) << " " << setw(10) << "-true " << endl;
-		else
-			cout << setw(10) << words[i] << setw(10) << " " << setw(10) << "-false " << endl;
-	}
+			for (int i = 0; i < 6; i++) {
+				bool check = checkWord(words[i]);
+				if (check)
+					cout << setw(10) << words[i] << setw(10) << " " << setw(10) << "-true " << endl;
+				else
+					cout << setw(10) << words[i] << setw(10) << " " << setw(10) << "-false " << endl;
+			}
+			break;
+		}
+		
+	} while(!check);
+
 
 	return 0;
 }
