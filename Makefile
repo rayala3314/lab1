96cbfa2ea5f0b77f4801cb1f738fd5ec3b170b9f
rayala3314 <rayala19@csub.edu>
2020-09-04 12:53:59 -0700

    my first commit to lab-1

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 #Raymundo Ayala
 
-all: lab1
+all: lab1 lab1_test
 
 lab1: lab1.cpp
 	g++ lab1.cpp -Wall -o lab1
+lab1_test: lab1.cpp
+	g++ lab1.cpp -Wall -o lab1_test -D UNIT_TEST
 
 clean:
-	rm -f lab1 
+	rm -f lab1 lab1_test
--- lab1.cpp
+++ lab1.cpp
@@ -6,35 +6,32 @@ using namespace std;
 
 bool checkWord(string word);
 
+
 int main(int argc, char* argv[])
 {
-	string words[6] = {"a", "aardvark", "book", "fall", "abcdddddd", "test"};
 	string word;
 	bool check = true;
+
+    #ifdef UNIT_TEST
+		string words[6] = {"a", "aardvark", "book", "fall", "abcdddddd", "test"};
+		for (int i = 0; i < 6; i++) {
+			bool check = checkWord(words[i]);
+			if (check)
+				cout << setw(10) << words[i] << setw(10) << " " << setw(10) << "-true " << endl;
+			else
+				cout << setw(10) << words[i] << setw(10) << " " << setw(10) << "-false " << endl;
+		}
+		
+		return 0;
+	#endif //UNIT_TEST
 	
     do {
-		if (argc > 1) {
-			for (int i = 0; i < argc; i++) {
-				if (strcmp(argv[i], "test")) {
-					for (int i = 0; i < 6; i++) {
-						bool check = checkWord(words[i]);
-						if (check)
-							cout << setw(10) << words[i] << setw(10) << " " << setw(10) << "-true " << endl;
-						else
-							cout << setw(10) << words[i] << setw(10) << " " << setw(10) << "-false " << endl;
-					}
-				}
+		cout << "Please enter a word: ";
+		cin >> word;
+		check = checkWord(word);
+		if (check)
+			cout << "Word has two consecutive letters that are the same\n";
 
-			}
-			break;
-		}
-		else {
-			cout << "Please enter a word: ";
-			cin >> word;
-			check = checkWord(word);
-			if (check)
-				cout << "Word has two consecutive letters that are the same\n";
-		}
 	} while(!check);
 
 
